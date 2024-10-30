880c4bac65919d78fbda48ca96c40f2796b31cfd
charckle <charckle@email.com>
2024-10-30 13:58:37 +0100

    version 0.0.2

--- web_manager/config.py
+++ web_manager/config.py
@@ -6,20 +6,20 @@ class Config(object):
     # environmental variables are set in .env, for development purpoises
     DEBUG = False
     TESTING = False
-    SECRET_KEY = environ.get('SECRET_KEY', "B\xb2?.\xdf\x9f\xa7m\xf8\x8a%,\xf7\xc4\xfa\x91")
+    SECRET_KEY = environ.get('SWM_SECRET_KEY', "B\xb2?.\xdf\x9f\xa7m\xf8\x8a%,\xf7\xc4\xfa\x91")
     
-    JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY', "sdf34tasdft34")
+    JWT_SECRET_KEY = environ.get('SWM_JWT_SECRET_KEY', "sdf34tasdft34")
     
-    APP_NAME = environ.get('APP_NAME', "Stribog Manager")
+    APP_NAME = environ.get('SWM_APP_NAME', "Stribog Manager")
     
-    ADMIN_USERNAME = environ.get('ADMIN_USERNAME', "admin")
+    ADMIN_USERNAME = environ.get('SWM_ADMIN_USERNAME', "admin")
     # SHA512
     # banana
-    ADMIN_PASS_HASH = environ.get('ADMIN_PASS_HASH', "$6$rounds=656000$Q3Z3OlpOU0VOPRCd$LIRME01nvrON4QNQX1tTYAcBVRhujpG2GNdzCJHmvSJDMN4ZJDYkm5r4qb9WGXW9cPoDCxWnXL43tqU6IezTw0")
+    ADMIN_PASS_HASH = environ.get('SWM_ADMIN_PASS_HASH', "$6$rounds=656000$Q3Z3OlpOU0VOPRCd$LIRME01nvrON4QNQX1tTYAcBVRhujpG2GNdzCJHmvSJDMN4ZJDYkm5r4qb9WGXW9cPoDCxWnXL43tqU6IezTw0")
     
     SESSION_COOKIE_SECURE = True
     
-    APP_LOGGING = environ.get('APP_LOGGING', "INFO")    
+    APP_LOGGING = environ.get('SWM_APP_LOGGING', "INFO")    
     
 
     # Application threads. A common general assumption is
