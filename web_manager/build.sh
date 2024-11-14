51b331360de8ecc9d477230bd41001e5911452ec
charckle <charckle@email.com>
2024-11-14 14:11:35 +0100

    added alpine image

--- web_manager/build.sh
+++ web_manager/build.sh
@@ -1,7 +1,13 @@
 # build image
-
-docker build -t $USERNAME/$IMAGE:$version .
+docker build -f Dockerfile-ubuntu -t $USERNAME/$IMAGE:$version .
 docker image tag $USERNAME/$IMAGE:$version $USERNAME/$IMAGE:latest
+
+docker build -f Dockerfile-alpine -t $USERNAME/$IMAGE:$version-alpine .
+docker image tag $USERNAME/$IMAGE:$version $USERNAME/$IMAGE:latest-alpine
+
+
 # push it to docker
 docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:latest-alpine
 docker push $USERNAME/$IMAGE:$version
+docker push $USERNAME/$IMAGE:$version-alpine
